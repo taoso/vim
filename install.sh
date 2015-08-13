305259c0615f26d044a74f38463e293dd2ffbe84
吕海涛 <git@lvht.net>
2015-08-13 23:50:00 +0800

    更新安装脚本

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-for name in viminfo nviminfo vimlog nvimlog vim nvim; do
-	rm ~/.$name
-done
+rm -rf ~/.vim*
+rm -rf ~/.nvim*
 
 git clone git://github.com/lvht/vim.git ~/.vim
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 ln -s ~/.vim ~/.nvim
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/vimrc ~/.nvimrc
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugInstall +qall
