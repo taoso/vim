08a7d52e3c04eb834868f46ffb1c7c4e674952b3
吕海涛 <git@lvht.net>
2015-11-02 11:32:42 +0800

    使用XDG标准路径

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.nvim*
+CONFIG_HOME=~/.config/nvim
+if [ ! -z $XDG_CONFIG_HOME ]; then
+	CONFIG_HOME=$XDG_CONFIG_HOME
+fi
 
-git clone https://github.com/lvht/vim.git ~/.nvim
-ln -s ~/.nvim/nvimrc ~/.nvimrc
-
-curl -fLo ~/.nvim/autoload/plug.vim --create-dirs \
+rm -rf $CONFIG_HOME/nvim
+git clone https://github.com/lvht/vim.git $CONFIG_HOME/nvim
+curl -fLo $CONFIG_HOME/nvim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 nvim +PlugInstall +qall
