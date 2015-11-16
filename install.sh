1832e413e14c8819379b213094d7c0ec93b98073
吕海涛 <git@lvht.net>
2015-11-16 10:22:34 +0800

    修复无法安装的问题

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-CONFIG_HOME=~/.config/nvim
 if [ ! -z $XDG_CONFIG_HOME ]; then
-	CONFIG_HOME=$XDG_CONFIG_HOME
+	CONFIG_HOME=$XDG_CONFIG_HOME/nvim
+else
+	CONFIG_HOME=~/.config/nvim
 fi
 
-rm -rf $CONFIG_HOME/nvim
-git clone https://github.com/lvht/vim.git $CONFIG_HOME/nvim
-curl -fLo $CONFIG_HOME/nvim/autoload/plug.vim --create-dirs \
+rm -rf $CONFIG_HOME
+git clone https://github.com/lvht/vim.git $CONFIG_HOME
+curl -fLo $CONFIG_HOME/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 nvim +PlugInstall +qall
