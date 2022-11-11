de816f656cc495090c8ff6925d57d78b3c30ccf6
吕海涛 <hi@taoshu.in>
2022-11-11 11:18:06 +0800

    Update pack

--- vim.lua
+++ vim.lua
@@ -121,8 +121,7 @@ cmp.setup {
     mapping = cmp.mapping.preset.insert(),
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require'cmp_nvim_lsp'.update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local servers = { 'gopls' }
 for _, lsp in ipairs(servers) do
