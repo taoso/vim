efaff6699e8c94aca3e83811f9222240fbd131a6
吕海涛 <hi@taoshu.in>
2022-12-30 13:45:22 +0800

    更新 tree 配置

--- vim.lua
+++ vim.lua
@@ -139,7 +139,12 @@ vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
+  hijack_cursor = true,
+  system_open = {
+    cmd = "open",
+  },
   view = {
+    width = 20,
     adaptive_size = true,
   },
   renderer = {
