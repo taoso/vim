f901b1c5bd79c5f63b44cfcd59b588eca5717878
吕海涛 <hi@taoshu.in>
2024-09-17 22:19:51 +0800

    drop treesitter-textobjects
    due to seldom usage

--- vim.lua
+++ vim.lua
@@ -8,61 +8,6 @@ require'nvim-treesitter.configs'.setup {
     indent = {
         enable = true,
     },
-    textobjects = {
-        select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-                ["aa"] = "@parameter.outer",
-                ["ia"] = "@parameter.inner",
-            },
-        },
-        move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-                ["]a"] = "@parameter.inner",
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-                ["]A"] = "@parameter.inner",
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-                ["[a"] = "@parameter.inner",
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-                ["[A"] = "@parameter.inner",
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
-            },
-        },
-        swap = {
-            enable = true,
-            swap_next = {
-                ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-                ["<leader>A"] = "@parameter.inner",
-            },
-        },
-        lsp_interop = {
-            enable = true,
-            border = 'none',
-            peek_definition_code = {
-                ["<leader>df"] = "@function.outer",
-                ["<leader>dF"] = "@class.outer",
-            },
-        },
-  },
 }
 
 vim.api.nvim_create_autocmd('LspAttach', {
